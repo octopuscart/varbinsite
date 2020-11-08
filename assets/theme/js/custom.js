01b93d27cebaa8c8ff0a8b6c2b41933b21f8a579
dev <dev@develop.com>
2020-11-08 15:36:05 +0530

    cahgns

--- assets/theme/js/custom.js
+++ assets/theme/js/custom.js
@@ -2,6 +2,7 @@ setTimeout(function(){
     jQuery(".hc-nav-trigger").attr("aria-label", "Mobile Menu");
     jQuery(".owl-dot").attr("aria-label", "Dot Navigation");
      jQuery("#mobilepopup").attr("aria-label", "Mobile Popup");
+     jQuery(".hc-nav-trigger").removeAttr("aria-controls");
     
    
 });
